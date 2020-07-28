feca31bf37d60285d723d54cac9b8337ed54079a
samraina04 <samraina@hotmail.com>
2020-07-29 01:00:22 +0530

    Login Component Added

--- src/User/Login/Login.js
+++ src/User/Login/Login.js
@@ -1,74 +1,115 @@
+import {Form, Button} from 'react-bootstrap'
 import React, { useState } from "react";
 import {Link} from "react-router-dom";
-// import Register from "../Register/Register"
-// import "../Register/register.css"
 // import "./login.css"
 
-function Login({match}) {
-    const[details, setDetails] = useState({
-        email: "", 
-        password: "", 
-        error: "",
-        success: "",
-        isAuth: ""
-    });
+// const LoginPage = () => {
+//     const[details, setDetails] = useState({
+//         email: "", 
+//         password: "", 
+//         error: "",
+//         success: "",
+//         isAuth: ""
+//     });
 
-    const { email, password, error, success, isAuth} = details;
+//     const { email, password, error, success, isAuth} = details;
 
-    const handleChange = detail => e => {
-        setDetails({...details, [detail]: e.target.value});
-    }
+//     const handleChange = detail => e => {
+//         setDetails({...details, [detail]: e.target.value});
+//     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Authenticate
-        // Redirect or Display Error Message
-    }
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         // Authenticate
+//         // Redirect or Display Error Message
+//     }
 
+//     return (
+//         <div className="loginpage-container">
+//             <div className="login-bg-img-container">
+//                 <div className="login-container">
+//                     <div className="text-center" id="login-heading">
+//                         <h1 className="display-4 heading-main">Sign In</h1>
+//                         <hr />
+//                     </div>
+//                     <form className="login-form" onSubmit={handleSubmit}>
+//                         <label htmlFor="email-input" className="lead">
+//                             Email-id
+//                         </label>
+//                         <input 
+//                         type="email" 
+//                         id="email-input" 
+//                         name="email"  
+//                         className="form-control"
+//                         autoComplete="email" 
+//                         pattern="email-id" 
+//                         autoFocus placeholder=" email-id"
+//                         value={email} 
+//                         onChange={handleChange("email")}
+//                         aria-describedby="emailHelp" 
+//                         />
+
+//                         <label htmlFor="password-input" className="lead">
+//                             Password
+//                         </label>
+//                         <input 
+//                         type="password" 
+//                         id="password-input" 
+//                         name="password" 
+//                         className="form-control" 
+//                         placeholder=" password" 
+//                         value={password} 
+//                         onChange={handleChange("password")}/>
+
+//                         <button 
+//                         type="submit" 
+//                         className="btn btn-primary"
+//                         id="signin-btn"
+//                         >
+//                             Sign In
+//                         </button>
+//                         <Link className="btn btn-primary" id="register-link" exact
+//                          to='/user/register'>
+//                             Register Here
+//                         </Link>
+//                     </form>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default LoginPage;
+const Login = () => {
 
     return (
-        <div className="login-container">
-            <div className="heading">
-                <h1 className="heading-main">Sign In</h1>
-                <hr className="heading-line" />
+        <div id="login-container">
+            <Form className="align-items-center">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+                </Form>
+                <Button variant="primary">
+                    <Link to={'/user/register'}>
+                        Register Here
+                    </Link>
+                </Button>
             </div>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email" className="details">
-                    Email-id
-                </label>
-                <input 
-                type="email" 
-                id="email" 
-                name="email"  
-                className="input-field"
-                autoComplete="email" 
-                pattern="email-id" 
-                autoFocus placeholder=" email-id" 
-                value={email} 
-                onChange={handleChange("email")} 
-                />
-
-                <label htmlFor="password" className="details">
-                    Password
-                </label>
-                <input 
-                type="password" 
-                id="password" 
-                name="password" 
-                className="input-field" 
-                placeholder=" password" 
-                value={password} 
-                onChange={handleChange(email)}/>
-
-                <button type="submit" className="button">
-                    Sign In
-                </button>
-
-                <Link className="button" id="reg-link" to='/user/register'>
-                    Register Here
-                </Link>
-            </form>
-        </div>
         );
 }
 
